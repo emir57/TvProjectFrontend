0bd62685d4a4b86656437cb380a8980cdcb16f19
emir57 <emir.gurbuz06@hotmail.com>
2021-12-05 15:18:24 +0300

    configure routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './Components/User/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"products",component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
