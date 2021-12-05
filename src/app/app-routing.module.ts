cd3b1bd82fdcf3ecc83fd1b8834a5172ed1e598c
emir57 <emir.gurbuz06@hotmail.com>
2021-12-05 16:33:59 +0300

    Add Categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './Components/User/category/category.component';
 import { ProductComponent } from './Components/User/product/product.component';
 
 const routes: Routes = [
   {path:"",component:ProductComponent},
-  {path:"products",component:ProductComponent}
+  {path:"products",component:ProductComponent},
+  // {path:"categories",component:CategoryComponent},
 ];
 
 @NgModule({
