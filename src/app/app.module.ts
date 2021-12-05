12761ced82d9a1cda9fd83b0b5b5c6963568fbca
emir57 <emir.gurbuz06@hotmail.com>
2021-12-05 15:11:14 +0300

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/User/home/home.component';
+import { CategoryComponent } from './Components/User/category/category.component';
+import { ProductComponent } from './Components/User/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoryComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
