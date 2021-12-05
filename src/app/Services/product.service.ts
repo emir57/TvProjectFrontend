8c76a0174afe5cf09a6b8e2b9a969390541e38f4
emir57 <emir.gurbuz06@hotmail.com>
2021-12-05 15:45:59 +0300

    Product Service Get Products

--- src/app/Services/product.service.ts
+++ src/app/Services/product.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiUrl } from '../Models/apiUrl';
+import { Product } from '../Models/product';
+import { ResponseListModel } from '../Models/responseListModel';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,9 @@ export class ProductService {
   constructor(
     private httpClient:HttpClient
   ) { }
-    getProducts():Observable<any>{
+    getProducts():Observable<ResponseListModel<Product>>{
       let newPath = `${this.apiUrl}/tvs/getall`;
+      return this.httpClient.get<ResponseListModel<Product>>(newPath);
     }
 
 }
