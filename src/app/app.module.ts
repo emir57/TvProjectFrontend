b9150227f18d47a71a29f90786892cc50f59990b
emir57 <emir.gurbuz06@hotmail.com>
2021-12-05 15:24:00 +0300

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/User/home/home.component';
 import { CategoryComponent } from './Components/User/category/category.component';
 import { ProductComponent } from './Components/User/product/product.component';
+import { NavbarComponent } from './Components/User/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ProductComponent } from './Components/User/product/product.component';
     HomeComponent,
     CategoryComponent,
     ProductComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
