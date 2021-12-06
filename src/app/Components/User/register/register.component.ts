a010e124094ba03d23609bb23eac6a4f488226e2
emir57 <emir.gurbuz06@hotmail.com>
2021-12-06 22:34:23 +0300

    Register Form

--- src/app/Components/User/register/register.component.ts
+++ src/app/Components/User/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required,Validators.maxLength(20)]],
+      lastName: ['', [Validators.required,Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email,Validators.maxLength(40)]],
+      password: ['', [Validators.required, Validators.minLength(5),Validators.maxLength(30)]],
+      agreement:[false]
+    })
+  }
+
+  register(){
+    if(this.registerForm.valid && this.registerForm.get('agreement').value){
+      console.log("başarılı")
+    }
   }
 
 }
+
+
