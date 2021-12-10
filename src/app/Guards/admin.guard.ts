d6e085dc30532a5c9e44c03e21832cda9db219d7
emir57 <emir.gurbuz06@hotmail.com>
2021-12-10 15:52:21 +0300

    Complete Admin Guard

--- src/app/Guards/admin.guard.ts
+++ src/app/Guards/admin.guard.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Role } from '../Models/role';
+import { User } from '../Models/user';
 import { AuthService } from '../Services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+  currentUser:User
+  roles:Role[]=[]
+  pageRole="Admin";
   constructor(
     private authService:AuthService
-  ){
-
-  }
+  ){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    this.currentUser=this.authService.getLoginUser();
+    this.authService.getUserRoles(this.currentUser.id)
+      .subscribe(response=>{
+        if(response.isSuccess){
+          this.roles=response.data;
+        }
+      })
+    if(this.authService.isInRole(this.roles,this.pageRole)){
+      return true;
+    }
+    return false;
   }
 
 }
