ff2f9e71fa1bbd523e95d79debd3e21c6330755e
emir57 <emir.gurbuz06@hotmail.com>
2021-12-11 18:52:47 +0300

    Admin Panel Items

---
+++ src/app/Models/adminPanelItems.ts
@@ -0,0 +1,9 @@
+export const AdminPanelItems=[
+  {id:1,displayName:"Ana Sayfa"},
+  {id:2,displayName:"Siparişler"},
+  {id:3,displayName:"Ürünler"},
+  {id:4,displayName:"Müşteriler"},
+  {id:5,displayName:"Markalar"},
+  {id:6,displayName:"Raporlar"},
+  {id:7,displayName:"Entegrasyonlar"}
+]
