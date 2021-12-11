5a4870d399cee975a62479243c8f1395e8ef0d73
emir57 <emir.gurbuz06@hotmail.com>
2021-12-11 20:39:11 +0300

    Create Form

--- src/app/Components/Admin/admin-product-add/admin-product-add.component.ts
+++ src/app/Components/Admin/admin-product-add/admin-product-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Category } from 'src/app/Models/category';
+import { CategoryService } from 'src/app/Services/category.service';
+import { ProductService } from 'src/app/Services/product.service';
 
 @Component({
   selector: 'app-admin-product-add',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductAddComponent implements OnInit {
 
-  constructor() { }
+  productAddForm:FormGroup
+  brands:Category[]=[]
+  constructor(
+    private formBuilder:FormBuilder,
+    private productService:ProductService,
+    private categoryService:CategoryService,
+    private toastrService:ToastrService
+  ) { }
 
   ngOnInit(): void {
+
+    this.createAddproductForm();
+  }
+
+  createAddproductForm(){
+    this.productAddForm = this.formBuilder.group({
+      productName:['',[Validators.required,Validators.maxLength(50),Validators.minLength(5)]],
+      productCode:['',[]],
+      screenType:['',[Validators.required,Validators.maxLength(50)]],
+      screenInch:['',[Validators.required,Validators.maxLength(10)]],
+      extras:['',[Validators.maxLength(50)]],
+      brandId:['',[Validators.required]],
+      unitPrice:['',[Validators.required,Validators.min(500)]],
+      discount:['',[]],
+      isDiscount:[false,[]],
+      stock:['',[Validators.required,Validators.min(1)]],
+    })
   }
 
 }
