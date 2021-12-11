c6bcd62ccc38a4f71c7756dc9a97c6d73aff797f
emir57 <emir.gurbuz06@hotmail.com>
2021-12-11 21:43:15 +0300

    Refactoring

--- src/app/Components/Admin/admin-product-add/admin-product-add.component.ts
+++ src/app/Components/Admin/admin-product-add/admin-product-add.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { $ } from 'protractor';
 import { Category } from 'src/app/Models/category';
+import { Product } from 'src/app/Models/product';
 import { CategoryService } from 'src/app/Services/category.service';
 import { ProductService } from 'src/app/Services/product.service';
 
@@ -38,9 +39,9 @@ export class AdminProductAddComponent implements OnInit {
       screenType:['',[Validators.required,Validators.maxLength(50)]],
       screenInch:['',[Validators.required,Validators.maxLength(10)]],
       extras:['',[Validators.maxLength(50)]],
-      brandId:['',[Validators.required]],
-      unitPrice:['',[Validators.required,Validators.min(500)]],
-      discount:['',[]],
+      brandId:[0,[Validators.required,Validators.min(1)]],
+      unitPrice:[,[Validators.required,Validators.min(500)]],
+      discount:[,[]],
       isDiscount:[false,[]],
       stock:['',[Validators.required,Validators.min(1),Validators.max(255)]],
     })
@@ -48,7 +49,15 @@ export class AdminProductAddComponent implements OnInit {
 
   addProduct(){
     if(this.productAddForm.valid){
-      console.log("Valid!");
+      this.productAddForm.get("brandId").setValue(+this.productAddForm.get("brandId").value)
+      console.log(this.productAddForm.value)
+      let productModel:Product = Object.assign({},this.productAddForm.value);
+      this.productService.addProduct(productModel).subscribe(response=>{
+        if(response.isSuccess){
+          this.toastrService.success(response.message);
+          this.productAddForm.reset;
+        }
+      })
     }
   }
 
