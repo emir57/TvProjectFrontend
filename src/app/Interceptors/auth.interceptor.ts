6365c10f483a8202e234bf63f84dc3a66145214c
emir57 <emir.gurbuz06@hotmail.com>
2021-12-11 20:42:26 +0300

    Interceptor

--- src/app/Interceptors/auth.interceptor.ts
+++ src/app/Interceptors/auth.interceptor.ts
@@ -13,7 +13,17 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
-    return next.handle(request);
+    let localToken = localStorage.getItem("token");
+    let sessionToken = sessionStorage.getItem("token");
+    let token = "";
+    if(localToken){
+      token = localToken
+    }else if(sessionToken){
+      token = sessionToken;
+    }
+    let newRequest = request.clone({
+      headers:request.headers.set("Authorization","Bearer "+token)
+    })
+    return next.handle(newRequest);
   }
 }
