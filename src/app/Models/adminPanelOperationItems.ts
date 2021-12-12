e9ca135170526df1d0f93511425c6327b0677c10
emir57 <emir.gurbuz06@hotmail.com>
2021-12-12 22:01:13 +0300

    Category Add Link

--- src/app/Models/adminPanelOperationItems.ts
+++ src/app/Models/adminPanelOperationItems.ts
@@ -2,5 +2,6 @@ import { AdminPanelItem } from "./adminPanelItem";
 
 export const AdminPanelOperationItems:AdminPanelItem[]=[
   {id:1,displayName:"<i class='fas fa-plus'></i> Ürün Ekle",link:"/admindashboard/productadd"},
+  {id:1,displayName:"<i class='fas fa-plus'></i> Kategori Ekle",link:"/admindashboard/categoryadd"},
   {id:2,displayName:"<i class='far fa-image'></i> Resim Yükle",link:"/admindashboard/uploadimage"},
 ]
