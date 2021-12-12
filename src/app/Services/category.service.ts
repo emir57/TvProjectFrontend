8e2784c4cddc50e4a26d09480af41e47d04ce0ce
emir57 <emir.gurbuz06@hotmail.com>
2021-12-12 22:18:56 +0300

    Add Category Operations

--- src/app/Services/category.service.ts
+++ src/app/Services/category.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { ApiUrl } from '../Models/apiUrl';
 import { Category } from '../Models/category';
 import { ResponseListModel } from '../Models/responseListModel';
+import { ResponseModel } from '../Models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,17 @@ export class CategoryService {
   getCategories():Observable<ResponseListModel<Category>>{
     let newPath = `${this.apiUrl}/api/tvbrands/getall`;
     return this.httpClient.get<ResponseListModel<Category>>(newPath);
+  }
+  addCategory(categoryModel:Category):Observable<ResponseModel>{
+    let newPath = `${this.apiUrl}/api/tvbrands/add`;
+    return this.httpClient.post<ResponseModel>(newPath,categoryModel);
+  }
+  updateCategory(categoryModel:Category):Observable<ResponseModel>{
+    let newPath = `${this.apiUrl}/api/tvbrands/update`;
+    return this.httpClient.put<ResponseModel>(newPath,categoryModel);
+  }
+  deleteCategory(categoryId:number):Observable<ResponseModel>{
+    let newPath = `${this.apiUrl}/api/tvbrands/delete/?tvBrandId=${categoryId}`;
+    return this.httpClient.delete<ResponseModel>(newPath);
   }
 }
