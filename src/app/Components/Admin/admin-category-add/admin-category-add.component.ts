ea2363e754b857a8ad2be66ed940bf4ccf3bbc7f
emir57 <emir.gurbuz06@hotmail.com>
2021-12-12 22:06:20 +0300

    Configure Ts

--- src/app/Components/Admin/admin-category-add/admin-category-add.component.ts
+++ src/app/Components/Admin/admin-category-add/admin-category-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Category } from 'src/app/Models/category';
+import { CategoryService } from 'src/app/Services/category.service';
 
 @Component({
   selector: 'app-admin-category-add',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCategoryAddComponent implements OnInit {
 
-  constructor() { }
+  isOk=true;
+  categoryAddForm:FormGroup
+  constructor(
+    private categoryService:CategoryService,
+    private activatedRouter:ActivatedRoute,
+    private router:Router,
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.createCategoryAddForm();
+  }
+
+  createCategoryAddForm(){
+    this.categoryAddForm = this.formBuilder.group({
+      name:['',[Validators.required,Validators.maxLength(50)]],
+      phoneNumber:['',[Validators.maxLength(15)]],
+      address:['',[Validators.maxLength(250)]]
+    })
+  }
+
+  addCategory(){
+    if(this.categoryAddForm.valid){
+      this.isOk=false;
+    }
   }
 
 }
