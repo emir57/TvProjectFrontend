044914c4b9691d54615da8687091512a72b84c9e
emir57 <emir.gurbuz06@hotmail.com>
2021-12-12 23:41:52 +0300

    Refactoring

--- src/app/Components/User/category/category.component.ts
+++ src/app/Components/User/category/category.component.ts
@@ -12,43 +12,44 @@ import { ProductService } from 'src/app/Services/product.service';
 export class CategoryComponent implements OnInit {
 
   categories: Category[] = [];
-  currentCategory: Category;
+  currentCategory: Category=null;
+  currentUrl =this.activatedRoute.snapshot.url;
   constructor(
     private categoryService: CategoryService,
+    private activatedRoute:ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.getCategories();
   }
-
   getCategories() {
     this.categoryService.getCategories()
       .subscribe(response => {
         this.categories = response.data;
       })
   }
-  setCurrentCategory(category: Category) {
-    this.currentCategory = category;
-  }
-
   getCurrentCategoryClass(category: Category) {
-    if (category == this.currentCategory) {
+    if (this.currentCategory==category) {
       return "list-group-item active";
     }
     else {
       return "list-group-item"
     }
   }
-  getAllCategoryClass(){
-    if(this.currentCategory==null){
-      return "list-group-item active";
-    }else{
-      return "list-group-item"
-    }
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
   }
+
   clearCategory(){
     this.currentCategory=null;
   }
 
 
+  getAllCategoryClass(){
+    if(this.currentUrl[0].path=="products"){
+      return "list-group-item active";
+    }else{
+      return "list-group-item"
+    }
+  }
 }
