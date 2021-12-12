f686f2a1d16991b1ef5bc77e2f85b90166e33c83
emir57 <emir.gurbuz06@hotmail.com>
2021-12-12 22:21:41 +0300

    Complete Category Add

--- src/app/Components/Admin/admin-category-add/admin-category-add.component.ts
+++ src/app/Components/Admin/admin-category-add/admin-category-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Category } from 'src/app/Models/category';
 import { CategoryService } from 'src/app/Services/category.service';
 
@@ -17,7 +18,8 @@ export class AdminCategoryAddComponent implements OnInit {
     private categoryService:CategoryService,
     private activatedRouter:ActivatedRoute,
     private router:Router,
-    private formBuilder:FormBuilder
+    private formBuilder:FormBuilder,
+    private toastrService:ToastrService
   ) { }
 
   ngOnInit(): void {
@@ -35,6 +37,16 @@ export class AdminCategoryAddComponent implements OnInit {
   addCategory(){
     if(this.categoryAddForm.valid){
       this.isOk=false;
+      let categoryModel = Object.assign({},this.categoryAddForm.value);
+      this.categoryService.addCategory(categoryModel).subscribe(response=>{
+        if(response.isSuccess){
+          this.toastrService.success(response.message);
+          this.categoryAddForm.reset();
+          this.isOk=true;
+        }else{
+          this.toastrService.error(response.message);
+        }
+      })
     }
   }
 
