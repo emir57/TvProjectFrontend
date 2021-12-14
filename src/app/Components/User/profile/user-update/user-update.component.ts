1017ba56ca23de033824d3de002d4a43061c687c
emir57 <emir.gurbuz06@hotmail.com>
2021-12-14 17:54:02 +0300

    Refactoring

--- src/app/Components/User/profile/user-update/user-update.component.ts
+++ src/app/Components/User/profile/user-update/user-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/Models/user';
 
 @Component({
   selector: 'app-user-update',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserUpdateComponent implements OnInit {
 
-  constructor() { }
+  isOk = true;
+  user: User
+  userUpdateForm: FormGroup;
+  resetPasswordForm: FormGroup
+  constructor(
+    private toastrService: ToastrService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    let sessionUser = sessionStorage.getItem("userInfo")
+    if (sessionUser) {
+      this.user = JSON.parse(sessionUser);
+    }
+    this.createUserUpdateForm();
+    this.createresetPasswordForm();
+  }
+
+  createUserUpdateForm() {
+    this.userUpdateForm = this.formBuilder.group({
+      firstName: [this.user.firstName, [Validators.required, Validators.maxLength(50)]],
+      lastName: [this.user.lastName, [Validators.required, Validators.maxLength(50)]],
+    })
+  }
+  createresetPasswordForm() {
+    this.resetPasswordForm = this.formBuilder.group({
+      oldPassword: ['', [Validators.required, Validators.maxLength(50)]],
+      newPassword: ['', [Validators.required, Validators.maxLength(50)]],
+    })
+  }
+
+  updateUserInfo() {
+    if (this.userUpdateForm.valid) {
+      this.isOk=false;
+    }
+  }
+  resetPassword() {
+    if (this.resetPasswordForm.valid) {
+      this.isOk=false;
+    }
   }
 
 }
