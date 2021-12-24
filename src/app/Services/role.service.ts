abfa9ecb174bfdb9cff6c188822774df895a5626
emir57 <emir.gurbuz06@hotmail.com>
2021-12-25 01:17:35 +0300

    Get Roles

--- src/app/Services/role.service.ts
+++ src/app/Services/role.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiUrl } from '../Models/apiUrl';
+import { ResponseListModel } from '../Models/responseListModel';
+import { Role } from '../Models/role';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleService {
 
-  constructor() { }
+  apiUrl=ApiUrl;
+  constructor(
+    private httpClient:HttpClient
+  ) { }
+
+  getRoles():Observable<ResponseListModel<Role>>{
+    let newPath = `${this.apiUrl}/api/roles/getall`;
+    return this.httpClient.get<ResponseListModel<Role>>(newPath);
+  }
 }
