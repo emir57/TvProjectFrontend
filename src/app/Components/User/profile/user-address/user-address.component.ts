18af103e5eea3eb48f4d9037367a8d7cfbbb0728
emir57 <emir.gurbuz06@hotmail.com>
2021-12-29 00:15:27 +0300

    Create Add Form

--- src/app/Components/User/profile/user-address/user-address.component.ts
+++ src/app/Components/User/profile/user-address/user-address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { UserAddressCityModel } from 'src/app/Models/userAddressCity';
 import { AddressService } from 'src/app/Services/address.service';
@@ -10,17 +11,28 @@ import { AddressService } from 'src/app/Services/address.service';
 })
 export class UserAddressComponent implements OnInit {
 
+  addForm:FormGroup
   addresses:UserAddressCityModel[]=[]
   userId:number=+sessionStorage.getItem("user")
   constructor(
     private addressService:AddressService,
-    private toastrService:ToastrService
+    private toastrService:ToastrService,
+    private formBuilder:FormBuilder,
   ) { }
 
   ngOnInit(): void {
     this.getAddress();
+    this.createAddForm();
   }
 
+  createAddForm(){
+    this.addForm = this.formBuilder.group({
+      addressName:['',[Validators.required,Validators.maxLength(20)]],
+      addressText:['',[Validators.required,Validators.maxLength(200)]],
+      userId:[this.userId,[]],
+      cityId:['',[Validators.required,Validators.min(1)]],
+    })
+  }
   getAddress(){
     this.addressService.getAddressesByUserId(this.userId).subscribe(response=>{
       if(response.isSuccess){
