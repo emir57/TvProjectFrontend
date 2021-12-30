311a8c92be22e79ceeafd3312abf388d17119b6c
emir57 <emir.gurbuz06@hotmail.com>
2021-12-30 16:11:39 +0300

    User Search Pipe

--- src/app/pipes/user-search.pipe.ts
+++ src/app/pipes/user-search.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { User } from '../Models/user';
 
 @Pipe({
   name: 'userSearch'
 })
 export class UserSearchPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: User[], searchString:string): User[] {
+    searchString = searchString ? searchString.toLocaleLowerCase():'';
+    return searchString?
+      value.filter(x=>x.firstName.toLocaleLowerCase().indexOf(searchString)!=-1||
+      x.lastName.toLocaleLowerCase().indexOf(searchString)!=-1||
+      x.email.toLocaleLowerCase().indexOf(searchString)!=-1):
+      value;
   }
 
 }
