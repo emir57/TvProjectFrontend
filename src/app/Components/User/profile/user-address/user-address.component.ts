f02da494ee4cf6f34283a46688f9c856fa1d4619
emir57 <emir.gurbuz06@hotmail.com>
2022-01-08 22:11:03 +0300

    close buttons

--- src/app/Components/User/profile/user-address/user-address.component.ts
+++ src/app/Components/User/profile/user-address/user-address.component.ts
@@ -142,5 +142,4 @@ export class UserAddressComponent implements OnInit {
       })
     }
   }
-
 }
--- src/app/Components/User/profile/user-orders/user-orders.component.ts
+++ src/app/Components/User/profile/user-orders/user-orders.component.ts
@@ -5,6 +5,7 @@ import { OrderModel } from 'src/app/Models/orderModel';
 import { Photo } from 'src/app/Models/photo';
 import { User } from 'src/app/Models/user';
 import { OrderService } from 'src/app/Services/order.service';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-user-orders',
@@ -13,50 +14,60 @@ import { OrderService } from 'src/app/Services/order.service';
 })
 export class UserOrdersComponent implements OnInit {
 
-  user:User;
-  orders:OrderModel[]=[]
+  user: User;
+  orders: OrderModel[] = []
   constructor(
-    private orderService:OrderService,
-    private toastrService:ToastrService
+    private orderService: OrderService,
+    private toastrService: ToastrService
   ) { }
 
   ngOnInit(): void {
     this.user = JSON.parse(sessionStorage.getItem("userInfo"));
     this.getOrders();
+
+    $(".deleteBoxClose").click(function () {
+      $("#deleteBox").fadeOut();
+      $("#deleteBoxBackground").fadeOut();
+    })
   }
 
-  getOrders(){
-    this.orderService.getOrdersByUser(this.user.id).subscribe(response=>{
-      if(response.isSuccess){
-        this.orders =response.data;
-      }else{
+  getOrders() {
+    this.orderService.getOrdersByUser(this.user.id).subscribe(response => {
+      if (response.isSuccess) {
+        this.orders = response.data;
+      } else {
         this.toastrService.error(response.message);
       }
     })
   }
 
-  getApiUrl(){
+  getApiUrl() {
     return ApiUrl
   }
-  getCount(){
+  getCount() {
     return this.orders.length
   }
-  deleteOrder(order:OrderModel){
-    if(confirm(`
+  deleteOrder(order: OrderModel) {
+    if (confirm(`
       Bu siparişi iptal etmek istediğinizden emin misiniz?\n
       ${order.tv.productName} ${order.tv.screenInch} ${order.tv.screenType}
-    `)){
-      this.orderService.deleteOrder(order.id).subscribe(response=>{
-        if(response.isSuccess){
+    `)) {
+      this.orderService.deleteOrder(order.id).subscribe(response => {
+        if (response.isSuccess) {
           this.toastrService.success(response.message);
           this.getOrders();
-        }else{
+        } else {
           this.toastrService.error(response.message)
         }
       })
-    }else{
+    } else {
       this.toastrService.info("Vazgeçildi")
     }
+  }
+
+  showAlertBox() {
+    $("#deleteBox").fadeIn();
+    $("#deleteBoxBackground").fadeIn();
   }
 
 }
