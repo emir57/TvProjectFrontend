df8713aa7cb83e340606dc77658b650b53583dc5
emir57 <emir.gurbuz06@hotmail.com>
2022-01-27 16:57:59 +0300

    Add Form

--- src/app/Components/Admin/admin-editcustomer/admin-editcustomer.component.ts
+++ src/app/Components/Admin/admin-editcustomer/admin-editcustomer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-editcustomer',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminEditcustomerComponent implements OnInit {
 
+  updateForm:FormGroup
   constructor() { }
 
   ngOnInit(): void {
