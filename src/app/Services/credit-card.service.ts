8e22f966051a4afb192ad7e0925b6d7b21628da6
emir57 <emir.gurbuz06@hotmail.com>
2022-02-06 13:43:00 +0300

    Add Credit card and delete

--- src/app/Services/credit-card.service.ts
+++ src/app/Services/credit-card.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiUrl } from '../Models/apiUrl';
+import { ResponseModel } from '../Models/responseModel';
+import { UserCreditCard } from '../Models/userCreditCard';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreditCardService {
 
-  constructor() { }
+  apiUrl=ApiUrl
+  constructor(
+    private httpClient:HttpClient
+  ) { }
+  add(creditCardModel:UserCreditCard):Observable<ResponseModel>{
+    let newPath = `${this.apiUrl}/api/creditCards/add`;
+    return this.httpClient.post<ResponseModel>("",creditCardModel);
+  }
+  delete(creditCardId:number):Observable<ResponseModel>{
+    let newPath = `${this.apiUrl}/api/creditCards/delete?id=${creditCardId}`;
+    return this.httpClient.delete<ResponseModel>(newPath);
+  }
 }
