e339356c2fd14ea3a785741134202e1638e8eb06
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-06 19:01:35 +0300

    Refactoring

--- src/app/Components/User/product/product.component.ts
+++ src/app/Components/User/product/product.component.ts
@@ -129,8 +129,6 @@ export class ProductComponent implements OnInit {
   }
 
   ImageSlide() {
-    // $("#spinnerDiv").fadeIn();
-    // $("#spinnerBgDiv").fadeIn();
     setTimeout(() => {
       function photosDisplayNone(photos) {
         for (let i = 0; i < photos.length; i++) {
