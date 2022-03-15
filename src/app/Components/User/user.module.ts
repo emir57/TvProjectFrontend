e49554c163fb732c39e1c7cee8d4b08fbc004320
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-15 15:00:15 +0300

    add module imports in user module

--- src/app/Components/User/user.module.ts
+++ src/app/Components/User/user.module.ts
@@ -1,6 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
+import { CategoryModule } from './category/category.module';
+import { CheckoutModule } from './checkout/checkout.module';
+import { FooterModule } from './footer/footer.module';
+import { ForgotpasswordModule } from './forgotpassword/forgotpassword.module';
+import { HomeModule } from './home/home.module';
+import { LoginModule } from './login/login.module';
+import { NavbarModule } from './navbar/navbar.module';
+import { ProductModule } from './product/product.module';
+import { ProfileModule } from './profile/profile.module';
+import { RegisterModule } from './register/register.module';
+import { ResetpasswordModule } from './resetpassword/resetpassword.module';
 
 
 
@@ -8,7 +19,17 @@ import { RegisterComponent } from './register/register.component';
   declarations: [],
   imports: [
     CommonModule,
-    RegisterComponent
+    CategoryModule,
+    CheckoutModule,
+    FooterModule,
+    ForgotpasswordModule,
+    HomeModule,
+    LoginModule,
+    NavbarModule,
+    ProductModule,
+    ProfileModule,
+    RegisterModule,
+    ResetpasswordModule
   ]
 })
 export class UserModule { }
