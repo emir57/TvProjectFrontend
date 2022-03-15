639ba527c6bf1a65e888cad53598dd31fc1333e0
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-15 15:28:45 +0300

    fix forgotpassword component

--- src/app/Components/User/forgotpassword/forgotpassword.module.ts
+++ src/app/Components/User/forgotpassword/forgotpassword.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForgotpasswordComponent } from './forgotpassword.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarModule } from '../navbar/navbar.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ForgotpasswordComponent
+  ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NavbarModule
   ]
 })
 export class ForgotpasswordModule { }
