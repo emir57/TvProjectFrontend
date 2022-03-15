87d21623d93c3763bc321136a645a09076f955ab
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-15 15:19:59 +0300

    Discount Pipe

--- src/app/pipes/discount/discount.module.ts
+++ src/app/pipes/discount/discount.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DiscountPipe } from './discount.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DiscountPipe
+  ],
   imports: [
     CommonModule
+  ],exports:[
+    DiscountPipe
   ]
 })
 export class DiscountModule { }
