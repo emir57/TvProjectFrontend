9728a3bf49bd82127c2cee985fbc9f9b245031c9
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-15 15:29:32 +0300

    fix login component

--- src/app/Components/User/login/login.module.ts
+++ src/app/Components/User/login/login.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarModule } from '../navbar/navbar.module';
+import { FooterModule } from '../footer/footer.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NavbarModule,
+    FooterModule
   ]
 })
 export class LoginModule { }
