a84795d34831405554e9398bea3e859f107250d8
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-15 15:28:09 +0300

    fix checkout component

--- src/app/Components/User/checkout/checkout.module.ts
+++ src/app/Components/User/checkout/checkout.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
+import { NavbarModule } from '../navbar/navbar.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CheckoutComponent
+  ],
   imports: [
     CommonModule,
+    NavbarModule
   ]
 })
 export class CheckoutModule { }
