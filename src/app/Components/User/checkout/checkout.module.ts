1810dc9b487dd32bffc4938d71a74f719eb8ba7f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-20 14:34:30 +0300

    fix disocunt module in checkout

--- src/app/Components/User/checkout/checkout.module.ts
+++ src/app/Components/User/checkout/checkout.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
 import { NavbarModule } from '../navbar/navbar.module';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { DiscountModule } from 'src/app/pipes/discount/discount.module';
 
 
 
@@ -13,7 +14,8 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
   imports: [
     CommonModule,
     AppRoutingModule,
-    NavbarModule
+    NavbarModule,
+    DiscountModule
   ]
 })
 export class CheckoutModule { }
