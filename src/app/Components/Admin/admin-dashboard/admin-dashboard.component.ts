84d8bd3a3f81eb24a624e3f7c9c6f3e181a3aa5b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-23 22:10:34 +0300

    add package ng2-charts

--- src/app/Components/Admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/Components/Admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -12,7 +13,50 @@ export class AdminDashboardComponent implements OnInit {
 
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    setTimeout(() => {
+      this.createCanv1();
+    }, 0);
+  }
+
+  createCanv1() {
+    const ctx = (document.getElementById('categoryProductCountCanv') as HTMLCanvasElement).getContext('2d');
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+
   }
 
 }
