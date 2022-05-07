5c1911cbe85465237351a12339bdc0372c3988f0
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-07 16:00:49 +0300

    ImageSlideModule-> declaration and export

--- src/app/image-slide/image-slide.module.ts
+++ src/app/image-slide/image-slide.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ImageSlideComponent } from './image-slide.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ImageSlideComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ImageSlideComponent
   ]
 })
 export class ImageSlideModule { }
