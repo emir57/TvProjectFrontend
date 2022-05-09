7ec1a31c18d2d95ff4a803ff36ba0c348cbf785d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-09 18:01:15 +0300

    refactoring delete alert service

--- src/app/Services/delete-alert.service.ts
+++ src/app/Services/delete-alert.service.ts
@@ -9,23 +9,26 @@ export class DeleteAlertService {
   constructor() { }
 
   showAlertBox(bodyText: string, successCallBack: () => void, errorCallBack: () => void) {
-    $("#deleteBox").fadeIn();
-    $("#deleteBoxBackground").fadeIn();
-    $("#deleteBoxText").html(bodyText)
+    const deleteBox = $("#deleteBox");
+    const deleteBoxBackground = $("#deleteBoxBackground");
+    const deleteBoxText = $("#deleteBoxText");
+    deleteBox.fadeIn();
+    deleteBoxBackground.fadeIn();
+    deleteBoxText.html(bodyText)
 
     $(".deleteBoxClose").click(function () {
-      $("#deleteBox").fadeOut();
-      $("#deleteBoxBackground").fadeOut();
+      deleteBox.fadeOut();
+      deleteBoxBackground.fadeOut();
       errorCallBack();
     })
-    $("#deleteBoxBackground").click(function () {
-      $("#deleteBox").fadeOut();
-      $("#deleteBoxBackground").fadeOut();
+    deleteBoxBackground.click(function () {
+      deleteBox.fadeOut();
+      deleteBoxBackground.fadeOut();
       errorCallBack();
     })
     $("#deleteBoxBtn").click(function () {
-      $("#deleteBox").fadeOut();
-      $("#deleteBoxBackground").fadeOut();
+      deleteBox.fadeOut();
+      deleteBoxBackground.fadeOut();
       successCallBack();
     })
   }
