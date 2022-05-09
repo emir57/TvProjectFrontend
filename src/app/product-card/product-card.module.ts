6acfdfcfb916a5b84b3489e7fd52cfa3c5949863
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-09 17:27:23 +0300

    import ImageSlideModule

--- src/app/product-card/product-card.module.ts
+++ src/app/product-card/product-card.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './product-card.component';
+import { ImageSlideModule } from '../image-slide/image-slide.module';
 
 
 
@@ -9,7 +10,8 @@ import { ProductCardComponent } from './product-card.component';
     ProductCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ImageSlideModule
   ], exports: [
     ProductCardComponent
   ]
