9f470a6269fca8c6a474def223c446fda302eb05
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-09 17:22:36 +0300

    product card input product

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductAndPhoto } from '../Models/productAndPhoto';
 
 @Component({
   selector: 'app-product-card',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
 
+  @Input() product: ProductAndPhoto;
   constructor() { }
 
   ngOnInit(): void {
