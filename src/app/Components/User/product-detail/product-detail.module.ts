3674669fd950b464c5104ed5fa638d5a91d8b846
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-17 13:57:16 +0300

    import imae slide module in product-detail module

--- src/app/Components/User/product-detail/product-detail.module.ts
+++ src/app/Components/User/product-detail/product-detail.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ImageSlideModule } from 'src/app/image-slide/image-slide.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ImageSlideModule
   ]
 })
 export class ProductDetailModule { }
