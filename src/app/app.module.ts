947a7c161c6c6299ce04fba500564fa19caeec02
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-17 14:29:37 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NgImageSliderModule } from 'ng-image-slider';
 import { UserModule } from './Components/User/user.module';
 import { AdminModule } from './Components/Admin/admin.module';
 import { RouterModule } from '@angular/router';
+import { ProductDetailModule } from './Components/user/product-detail/product-detail.module';
 
 
 @NgModule({
