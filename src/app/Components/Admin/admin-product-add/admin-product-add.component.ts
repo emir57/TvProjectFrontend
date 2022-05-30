48bbbcf8429a0264592796f4564a4d23e5dc2e7c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-30 14:45:08 +0300

    admin product add page back button refactoring

--- src/app/Components/Admin/admin-product-add/admin-product-add.component.ts
+++ src/app/Components/Admin/admin-product-add/admin-product-add.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
@@ -14,55 +15,60 @@ import { ProductService } from 'src/app/Services/product.service';
 })
 export class AdminProductAddComponent implements OnInit {
 
-  isOk=true;
-  productAddForm:FormGroup;
-  categories:Category[]=[];
-  selectedCategory:Category;
+  isOk = true;
+  productAddForm: FormGroup;
+  categories: Category[] = [];
+  selectedCategory: Category;
   constructor(
-    private formBuilder:FormBuilder,
-    private productService:ProductService,
-    private categoryService:CategoryService,
-    private toastrService:ToastrService
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private categoryService: CategoryService,
+    private toastrService: ToastrService,
+    private location: Location
   ) { }
 
+  back() {
+    this.location.back();
+  }
+
   ngOnInit(): void {
-    this.categoryService.getCategories().subscribe(response=>{
-      if(response.isSuccess){
+    this.categoryService.getCategories().subscribe(response => {
+      if (response.isSuccess) {
         this.categories = response.data;
       }
     })
     this.createAddproductForm();
   }
-  createAddproductForm(){
+  createAddproductForm() {
     this.productAddForm = this.formBuilder.group({
-      productName:['',[Validators.required,Validators.maxLength(50),Validators.minLength(5)]],
-      productCode:['',[]],
-      screenType:['',[Validators.required,Validators.maxLength(50)]],
-      screenInch:['',[Validators.required,Validators.maxLength(10)]],
-      extras:['',[Validators.maxLength(50)]],
-      brandId:[0,[Validators.required,Validators.min(1)]],
-      unitPrice:[,[Validators.required,Validators.min(500)]],
-      discount:[,[]],
-      isDiscount:[false,[]],
-      stock:['',[Validators.required,Validators.min(1),Validators.max(255)]],
+      productName: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(5)]],
+      productCode: ['', []],
+      screenType: ['', [Validators.required, Validators.maxLength(50)]],
+      screenInch: ['', [Validators.required, Validators.maxLength(10)]],
+      extras: ['', [Validators.maxLength(50)]],
+      brandId: [0, [Validators.required, Validators.min(1)]],
+      unitPrice: [, [Validators.required, Validators.min(500)]],
+      discount: [, []],
+      isDiscount: [false, []],
+      stock: ['', [Validators.required, Validators.min(1), Validators.max(255)]],
     })
   }
-  selectCategory(brandId:any){
+  selectCategory(brandId: any) {
     console.log(brandId.value)
-    this.categoryService.getCategories().subscribe(response=>{
-      response.data.forEach(category=>{
-        if(category.id == brandId.value){
+    this.categoryService.getCategories().subscribe(response => {
+      response.data.forEach(category => {
+        if (category.id == brandId.value) {
           this.selectedCategory = category;
         }
       })
     })
   }
 
-  addProduct(){
-    if(this.productAddForm.valid){
-      this.isOk=false;
+  addProduct() {
+    if (this.productAddForm.valid) {
+      this.isOk = false;
       this.productAddForm.get("brandId").setValue(+this.productAddForm.get("brandId").value)
-      let productModel:Product = Object.assign({},this.productAddForm.value);
+      let productModel: Product = Object.assign({}, this.productAddForm.value);
       console.log(productModel);
       // this.productService.addProduct(productModel).subscribe(response=>{
       //   if(response.isSuccess){
