6f391fd102229ad717685980fc49dd915f506d4b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-01 15:24:19 +0300

    create upload images functionc

--- src/app/Services/photo.service.ts
+++ src/app/Services/photo.service.ts
@@ -10,17 +10,27 @@ import { ResponseModel } from '../Models/response/responseModel';
 })
 export class PhotoService {
 
-  apiUrl=`${ApiUrl}/api/photos/upload`
+  apiUrl = `${ApiUrl}/api/photos/upload`
   constructor(
-    private httpClient:HttpClient
+    private httpClient: HttpClient
   ) { }
 
-  uploadImage(image:File,photoModel:Photo):Observable<ResponseModel>{
+  uploadImage(image: File, photoModel: Photo): Observable<ResponseModel> {
     const sendForm = new FormData();
-    sendForm.append("file",image,image.name);
-    sendForm.append("tvid",JSON.stringify(photoModel.tvId));
-    sendForm.append("ismain",JSON.stringify(photoModel.isMain));
+    sendForm.append("file", image, image.name);
+    sendForm.append("tvid", JSON.stringify(photoModel.tvId));
+    sendForm.append("ismain", JSON.stringify(photoModel.isMain));
     // sendForm.append("",JSON.stringify(photoModel));
-    return this.httpClient.post<ResponseModel>(this.apiUrl,sendForm)
+    return this.httpClient.post<ResponseModel>(this.apiUrl, sendForm)
+  }
+  uploadImages(images: File[], photoModel: Photo): Observable<ResponseModel> {
+    const sendForm = new FormData();
+    images.forEach(image => {
+      sendForm.append("files", image, image.name);
+    });
+    sendForm.append("tvid", JSON.stringify(photoModel.tvId));
+    sendForm.append("ismain", JSON.stringify(photoModel.isMain));
+    // sendForm.append("",JSON.stringify(photoModel));
+    return this.httpClient.post<ResponseModel>(this.apiUrl, sendForm)
   }
 }
