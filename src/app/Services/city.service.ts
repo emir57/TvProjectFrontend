cff84b122943da955c02d8c452becbb2b8124a7e
emir57 <emir.gurbuz06@hotmail.com>
2022-06-25 18:54:54 +0300

    getCity configure path

--- src/app/Services/city.service.ts
+++ src/app/Services/city.service.ts
@@ -21,7 +21,7 @@ export class CityService {
     return this.httpClient.get<ResponseListModel<City>>(newPath);
   }
   getCity(id: number): Observable<ResponseSingleModel<City>> {
-    let newPath = `${this.apiUrl}/api/cities/id=${id}`;
+    let newPath = `${this.apiUrl}/api/cities/${id}`;
     return this.httpClient.get<ResponseSingleModel<City>>(newPath);
   }
   updateCity(city: City): Observable<ResponseModel> {
