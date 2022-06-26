9c90800c0d2891fbbaa922364b3a4b3fedcf5983
emir57 <emir.gurbuz06@hotmail.com>
2022-06-26 17:40:03 +0300

    category service configure paths

--- src/app/Services/category.service.ts
+++ src/app/Services/category.service.ts
@@ -20,7 +20,7 @@ export class CategoryService {
   ) { }
 
   getCategories(): Observable<ResponseListModel<Category>> {
-    let newPath = `${this.apiUrl}/api/tvbrands/getall`;
+    let newPath = `${this.apiUrl}/api/tvbrands`;
     return this.httpClient.get<ResponseListModel<Category>>(newPath);
   }
   getAllWithCount(): Observable<ResponseListModel<CategoryWithCount>> {
@@ -32,19 +32,19 @@ export class CategoryService {
     return this.httpClient.get<ResponseListModel<CategoryWithPriceAverage>>(newPath);
   }
   getCategoryById(id: number): Observable<ResponseSingleModel<Category>> {
-    let newPath = `${this.apiUrl}/api/tvbrands/getbyid?id=${id}`;
+    let newPath = `${this.apiUrl}/api/tvbrands/${id}`;
     return this.httpClient.get<ResponseSingleModel<Category>>(newPath);
   }
   addCategory(categoryModel: Category): Observable<ResponseModel> {
-    let newPath = `${this.apiUrl}/api/tvbrands/add`;
+    let newPath = `${this.apiUrl}/api/tvbrands`;
     return this.httpClient.post<ResponseModel>(newPath, categoryModel);
   }
   updateCategory(categoryModel: Category): Observable<ResponseModel> {
-    let newPath = `${this.apiUrl}/api/tvbrands/update`;
+    let newPath = `${this.apiUrl}/api/tvbrands`;
     return this.httpClient.put<ResponseModel>(newPath, categoryModel);
   }
   deleteCategory(categoryId: number): Observable<ResponseModel> {
-    let newPath = `${this.apiUrl}/api/tvbrands/delete/?tvBrandId=${categoryId}`;
+    let newPath = `${this.apiUrl}/api/tvbrands/${categoryId}`;
     return this.httpClient.delete<ResponseModel>(newPath);
   }
 }
